e0c7cb3f4cbabe5c088ce7924c9288687529cf17
Manivasagam Sivaraman <manivasagamsivaraman@Manivasagams-MacBook-Pro.local>
2021-04-17 21:05:04 -0500

    first commit

--- App.js
+++ App.js
@@ -1,109 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard } from 'react-native';
-import Task from './components/Task';
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation';
+import home from './myScreens/Main'
+import lis from './myScreens/Todo'
+export default class AppContainer extends React.Component {
 
-export default function App() {
+  render() {
   
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
-
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task]);
-    setTask(null);
-  }
-
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
-  }
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}> Today's tasks </Text>
-
-        <View style={styles.items}>
-          {/* whjere styles will go */}
-          {
-            taskItems.map((item, index) => {
-             return (
-               <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                  <Task text={item} />
-               </TouchableOpacity>
-             ) 
-             
-            }) 
+        const AppNavigator = createStackNavigator({
+          Primary: {
+            screen: home
+          },
+          ToDo: {
+            screen: lis
           }
-          
-        </View>
-
-      </View>
-
-
-      {/*writing a task, like actually making it*/}
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : height}
-        style={styles.writeTaskWrapper}
-        >
-          <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
-          <TouchableOpacity onPress={()=> handleAddTask()}>
-            <View style={styles.addWrapper}>
-              <Text style={styles.addText}>+</Text>
-            </View>
-          </TouchableOpacity>
-      </KeyboardAvoidingView>
-
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E8EAED',
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold', 
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-  },
-  addText: {},
+        },
+      
+          {
+            initialRouteName: 'home',
+            headerMode: 'none'
+          });
   
-});
+        const AppContainer = createAppContainer(AppNavigator);
+        return(
+        <AppContainer/>
+        );
+    }
+  
+  
+  }
